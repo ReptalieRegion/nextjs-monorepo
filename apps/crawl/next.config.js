b1f934c472900962534ff34b22cd9e84055f7195
yunchanpark <yongp99@nate.com>
2023-12-29 19:40:18 +0900

    add: crawl 이미지 추가 및 deploy 스크립트 추가

--- apps/crawl/next.config.js
+++ apps/crawl/next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@repo/ui"],
+    transpilePackages: ['@repo/ui'],
+    webpack: (config) => {
+        // 기존 URL 파일로더 규칙을 가져옵니다.
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+        // 기존 파일로더에 새로 추가한 `@svgr/webpack` 규칙을 추가합니다.
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                // 후에 설명할 리소스 쿼리입니다.
+                resourceQuery: { not: /components/ },
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                resourceQuery: /components/,
+                use: ['@svgr/webpack'],
+            },
+        );
+        config.resolve.alias.canvas = false;
+        return config;
+    },
 };
