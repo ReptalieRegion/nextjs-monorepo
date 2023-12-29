1e60244a26ef2c9429a6a056bc556614956a09cb
yunchanpark <yongp99@nate.com>
2023-12-30 01:00:02 +0900

    fix: docker .next/static  폴더 위치 수정

--- apps/crawl/src/components/NotionPage/NotionRenderPage.tsx
+++ apps/crawl/src/components/NotionPage/NotionRenderPage.tsx
@@ -6,21 +6,11 @@ import Link from 'next/link';
 import type { ExtendedRecordMap } from 'notion-types';
 import { NotionRenderer } from 'react-notion-x';
 
-const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code), {
-    ssr: false,
-});
-const Collection = dynamic(() => import('react-notion-x/build/third-party/collection').then((m) => m.Collection), {
-    ssr: false,
-});
-const Equation = dynamic(() => import('react-notion-x/build/third-party/equation').then((m) => m.Equation), {
-    ssr: false,
-});
-const Pdf = dynamic(() => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf), {
-    ssr: false,
-});
-const Modal = dynamic(() => import('react-notion-x/build/third-party/modal').then((m) => m.Modal), {
-    ssr: false,
-});
+const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code));
+const Collection = dynamic(() => import('react-notion-x/build/third-party/collection').then((m) => m.Collection));
+const Equation = dynamic(() => import('react-notion-x/build/third-party/equation').then((m) => m.Equation));
+const Pdf = dynamic(() => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf));
+const Modal = dynamic(() => import('react-notion-x/build/third-party/modal').then((m) => m.Modal));
 
 type NotionPageProps = {
     recordMap: ExtendedRecordMap;
