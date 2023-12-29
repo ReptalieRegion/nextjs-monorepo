84dd5d421324681c97f2cb6ef9709d68213acac2
yunchanpark <yongp99@nate.com>
2023-12-29 19:22:47 +0900

    add: prettier 세팅

--- apps/crawl/src/app/page.tsx
+++ apps/crawl/src/app/page.tsx
@@ -1,11 +1,10 @@
-import Link from "next/link";
-
+import Link from 'next/link';
 
 export default function Page(): JSX.Element {
-  return (
-    <main>
-      <Link href={'./privacy-policy'}>개인정보 처리 방칭</Link>
-      <Link href={'./privacy-policy'}>actions</Link>
-    </main>
-  );
+    return (
+        <main>
+            <Link href={'./privacy-policy'}>개인정보 처리 방칭</Link>
+            <Link href={'./privacy-policy'}>actions</Link>
+        </main>
+    );
 }
---
+++ packages/prettierrc-config/index.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+    printWidth: 128,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    semi: true,
+    jsxBracketSameLine: false,
+};
+
+export default config;
