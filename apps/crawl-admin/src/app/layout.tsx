ddba055668e33ccb64382a1cded45d7648c88736
yunchanpark <yongp99@nate.com>
2024-02-21 18:55:26 +0900

    add: nextui 설정

--- apps/crawl-admin/src/app/layout.tsx
+++ apps/crawl-admin/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { NextUIProvider } from "@nextui-org/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,7 +19,9 @@ export default async function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <NextUIProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </NextUIProvider>
       </body>
     </html>
   );
